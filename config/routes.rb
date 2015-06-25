27e70e2b5a1cdae3cb26086f7f7b472f17f9fb3b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-25 09:42:06 +0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home_page#home'
 
-  get '/contact', to: 'home_page#home', as: 'contact'
+  get '/contact', to: 'home_page#contact', as: 'contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
