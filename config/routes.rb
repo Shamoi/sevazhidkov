72be78916792b05d971407438563b3430f3f3114
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-25 09:39:24 +0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home_page/home'
 
-  get 'home_page/contact'
+  get '/contact', to: 'home_page#home', as: 'contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
