b3484733fd6604378fcdd591013752f5cdc1596c
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 12:00:32 +0400

    Redirect to root_path after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = Admin.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Lulz
+      sign_in user
+      flash[:success] = "Вы успешно вошли в админ-панель"
+      redirect_to root_path # In future, change it to admin path
     else
       flash[:error] = "Неправильный логин/пароль"
       redirect_to login_path
