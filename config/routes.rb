b9b6fd6c9a458abe28d825ff8e4780f14d17bf2f
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 12:08:03 +0400

    Add namespace admin to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   match '/admin/login',       to: 'sessions#new',     via: 'get',    as: 'login'
   match '/admin/logout',      to: 'sessions#destroy', via: 'delete'
 
+  namespace :admin do
+    get '/', to: 'admin#apps', as: 'apps'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
