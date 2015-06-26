68bf6c818f6ddd6e95c1ca9dc4b86eedf60dc153
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 16:39:51 +0400

    Generate services controller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,19 @@
+class ServicesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def list
+  end
+
+  def show
+  end
+end
