fe82a64f79ac7c4755e38b423c4f8d65f57ac49b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 08:58:31 +0400

    Add Service model

---
+++ db/migrate/20150626045748_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration
+  def change
+    create_table :services do |t|
+      t.string :name
+      t.string :description
+      t.string :link
+      t.string :admin_link
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626043527) do
+ActiveRecord::Schema.define(version: 20150626045748) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20150626043527) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "link"
+    t.string   "admin_link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
