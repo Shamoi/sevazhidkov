cbd7d0e7cd5bce274b0656250404c63f569a2150
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 18:26:18 +0400

    Add edit action

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,10 @@
 class ServicesController < ApplicationController
   before_action :redirect_not_admin
-  
+
+  def index
+    @services = Service.all
+  end
+
   def new
   end
 
@@ -11,10 +15,19 @@ class ServicesController < ApplicationController
   end
 
   def edit
+    @service = Service.find(params[:id])
   end
 
-  def index
-    @services = Service.all
+  def update
+    @service = Service.find(params[:id])
+    service_attributes = params[:service].permit(:name, :description,
+                                                 :link, :admin_link  )
+    if @service.update_attributes(service_attributes)
+      flash[:success] = "Сервис обновлен"
+      redirect_to @service
+    else
+      redirect_to 'edit'
+    end
   end
 
   def show
