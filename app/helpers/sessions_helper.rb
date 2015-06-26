101f20d50a4842634924bbd5742eb2ccf2b9690d
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 15:10:45 +0400

    Fix typo error in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(admin)
     remember_token = Admin.new_remember_token
-    cookies.permanent[:remmber_token] = remember_token
+    cookies.permanent[:remember_token] = remember_token
     admin.update_attribute(:remember_token, Admin.encrypt(remember_token))
     self.current_admin = admin
   end
