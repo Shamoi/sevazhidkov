6df1c57919883e63f41c1b482102660f12a6db60
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 12:06:18 +0400

    Add check for admin in apps action of admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
   def apps
+    if !admin?
+      flash[:error] = "Необходимо авторизоваться, чтобы использовать админку"
+      redirect_to login_path
   end
 end
