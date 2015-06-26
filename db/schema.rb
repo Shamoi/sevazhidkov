003379f1b98134003c5c81813731d540e6a5c58a
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-26 11:20:36 +0400

    Add remember_token column to admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626050042) do
+ActiveRecord::Schema.define(version: 20150626071832) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150626050042) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_token"
   end
+
+  add_index "admins", ["remember_token"], name: "index_admins_on_remember_token"
 
   create_table "services", force: :cascade do |t|
     t.string   "name"
