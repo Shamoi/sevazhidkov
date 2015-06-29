057e0bdf36b5aad35d37a900194e2c691faee687
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-29 10:50:24 +0400

    Add index articles action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all.order_by(:updated_at)
+  end
+
   private
     def article_attributes
       params.require(:article).permit(:title, :text)
