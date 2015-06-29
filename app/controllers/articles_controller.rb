ac654aa6e6621424e82200bdc27f5acfd4732313
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-29 10:19:00 +0400

    Create show action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_attributes
       params.require(:article).permit(:title, :text)
