d7c9465f4cbcd51e3664eb2f24461438286c7bc4
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-29 12:43:32 +0400

    Add column description to articles model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629051131) do
+ActiveRecord::Schema.define(version: 20150629084210) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20150629051131) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "services", force: :cascade do |t|
