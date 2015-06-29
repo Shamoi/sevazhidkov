9d6494d6fed3ad6dd9ed28bb1624a2186b1884fb
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-29 10:16:37 +0400

    Create 'create new article' action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_attributes)
+    if @article.save
+      flash[:success] = "Публикация создана"
+      redirect_to @article
+    else
+      flash[:danger] = "Произошла ошибка во время публикации. Попробуйте еще раз."
+      redirect_to new_article_path
+  end
+
+  private
+    def article_attributes
+      params.require(:article).permit(:title, :text)
+    end
 end
