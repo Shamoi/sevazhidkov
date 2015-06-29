12b761c24b22e54fa475cff259d301397f91bdbc
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-29 09:15:09 +0400

    Add articles resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 
   resources :services
   resources :admins, only: [:new, :create, :destroy, :index, :update, :edit]
+
+  # Services
+
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
