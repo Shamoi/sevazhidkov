ebf71ccbbc29645194f0ea3a5eda97287510b915
Seva Zhidkov <zhidkovseva@gmail.com>
2015-06-30 20:39:47 +0400

    Translate markdown to html in articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
+  after_validation :create_html_from_markdown, on: [ :create, :update ]
+
   validates :title, :text,
             presence: true
+
+  protected
+    def create_html_from_markdown
+      self.text = Markdown.new(self.text).to_html
+    end
 end
