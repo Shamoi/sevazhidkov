582518bb2e0c01eb9f3b6cbccee37127f87210f8
Seva Zhidkov <zhidkovseva@gmail.com>
2015-07-01 10:19:17 +0400

    Generate journal model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630124514) do
+ActiveRecord::Schema.define(version: 20150701061822) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150630124514) do
     t.datetime "updated_at",  null: false
     t.text     "description"
     t.decimal  "likes"
+  end
+
+  create_table "journals", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "services", force: :cascade do |t|
