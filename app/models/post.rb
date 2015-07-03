add6633b2218d13153294b7c92b86a9514107771
Seva Zhidkov <zhidkovseva@gmail.com>
2015-07-03 16:25:05 +0400

    Generate html from post text markdown

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
+  after_validation :create_html_from_markdown, on: [ :create, :update ]
+
   belongs_to :journal
+
+  protected
+    def create_html_from_markdown
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                         autolink: true, tables: true)
+      self.text = markdown.render(self.text)
+    end
 end
