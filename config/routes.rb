3a9105c66477dd1a2b8d1c330777d12fddc2ba64
Seva Zhidkov <zhidkovseva@gmail.com>
2015-07-03 11:44:52 +0400

    Update routes for journals controller

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 
   # Journals
   resources :journals
+  get '/:name', to: 'journals#show', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
