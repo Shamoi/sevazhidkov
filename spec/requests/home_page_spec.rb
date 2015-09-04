67be6a2f8d572b02d30a618aab222761f69e89ed
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-04 18:54:47 +0400

    Fix typo in home page spec

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Home page" do
-  it "should return OK status code" do
+  it "should return 'OK' status code" do
     visit '/'
     response.status.should be(200)
   end
