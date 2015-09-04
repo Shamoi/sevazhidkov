c3046cc0386cac9ffcf1a46448eed3f0ddaf1147
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-04 21:21:17 +0400

    Fix test for owner's name in layout

--- spec/requests/layout_spec.rb
+++ spec/requests/layout_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe "Layout" do
-  describe "Header" do
-    it "should have owner's name as heading" do
+  describe "header" do
+    it "should have owner's name" do
       # Home page have all layout
       visit '/'
-      except(page).to have_content('<h1>Сева Жидков</h1>')
+      expect(page).to have_content('Сева Жидков')
     end
   end
 end
