52390fe41cc8961cdb1c54284d02c995f7a9a18f
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-04 22:02:16 +0400

    Add tests for copyright in footer

--- spec/requests/layout_spec.rb
+++ spec/requests/layout_spec.rb
@@ -13,4 +13,18 @@ describe "Layout" do
       expect(page).to have_content('Люблю программирование и себя')
     end
   end
+
+  describe "footer" do
+    it "should have copyright" do
+      # Home page have all layout
+      visit '/'
+      expect(page).to have_content('© 2015. Сева Жидков.')
+    end
+
+    it "should have link to git repo" do
+      # Home page have all layout
+      visit '/'
+      expect(page).to have_link('https://github.com/sevazhidkov/sevazhidkov')
+    end
+  end
 end
