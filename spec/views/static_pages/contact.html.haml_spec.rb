2156c5d329c8393bea5700ac6869730e1736e228
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-05 08:57:01 +0400

    Fix test for contact page view

--- spec/views/static_pages/contact.html.haml_spec.rb
+++ spec/views/static_pages/contact.html.haml_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe "static_pages/contact.html.haml" do
   it 'should have links to my social networks' do
+    social_networks = {
+      'Twitter' => 'https://twitter.com/shamoiseva',
+      'VK' => 'https://vk.com/shamoiseva',
+      'Instagram' => 'https://instagram.com/lifeofshamoi/'
+    }
+
+    assign(:social_networks, social_networks)
+
     render
 
-    social_networks_urls = ['https://vk.com/shamoiseva',
-                            'https://twitter.com/shamoiseva',
-                            'https://instagram.com/lifeofshamoi/']
-    social_networks_urls.each do |network_url|
-      expect(rendered).to have_link(href: network_url)
+    social_networks.each do |name, url|
+      expect(rendered).to have_link(name, href: url)
     end
   end
 end
