34f7a02967a179285b9f3fb5949d6b950d505cfa
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-05 21:48:33 +0400

    Add test for link to index page

--- spec/views/layouts/_navigation.html.haml_spec.rb
+++ spec/views/layouts/_navigation.html.haml_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe "layouts/_navigation.html.haml" do
+  it "should have link for index page" do
+    render
+    expect(rendered).to have_link('Главная', '/')
+  end
+
   it "should have link for contacts page" do
     render
     expect(rendered).to have_link('Контакты', '/contacts')
