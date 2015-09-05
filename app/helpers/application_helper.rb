5964a69859ef1f24ab5ea1a8d2ba6a577b276418
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-05 18:33:49 +0400

    Create helper for getting full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def get_full_title(page_name)
+    basic_title = 'Сева Жидков'
+    if page_name
+      return page_name + ' — ' + basic_title
+    else
+      return basic_title
+    end
+  end
 end
