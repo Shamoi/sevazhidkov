7577733502c5ba5a0739ac181a81d433c4af7f62
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-05 08:27:56 +0400

    Add view test for links to my social networks

--- spec/views/static_pages/contact.html.haml_spec.rb
+++ spec/views/static_pages/contact.html.haml_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe "static_pages/contact.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have links to my social networks' do
+    render
+
+    social_networks_urls = ['https://vk.com/shamoiseva',
+                            'https://twitter.com/shamoiseva',
+                            'https://instagram.com/lifeofshamoi/']
+    social_networks_urls.each do |network_url|
+      except(rendered).to have_link(href: network_url)
+    end
+  end
 end
