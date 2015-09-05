796e94839ac6779c5b5c75cffc26ba62ed2fa88c
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-05 18:45:42 +0400

    Fix getting full title without page name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def get_full_title(page_name)
+  def get_full_title(page_name=nil)
     basic_title = 'Сева Жидков'
     if page_name
       return page_name + ' — ' + basic_title
