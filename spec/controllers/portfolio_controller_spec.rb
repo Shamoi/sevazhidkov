9cf94c1b184054f8feeb0b4e9a182ce8771a5f48
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-06 16:40:26 +0400

    Generate tests for portfolio

---
+++ spec/controllers/portfolio_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe PortfolioController do
+
+  describe "index project" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "show project" do
+    it "returns http success" do
+      get 'show'
+      response.should be_success
+    end
+  end
+
+  describe "create project" do
+    it "returns http success" do
+      post 'create'
+      response.should be_success
+    end
+  end
+
+  describe "new project" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "edit project" do
+    it "returns http success" do
+      get 'edit'
+      response.should be_success
+    end
+  end
+
+  describe "update project" do
+    it "returns http success" do
+      post 'update'
+      response.should be_success
+    end
+  end
+
+  describe "destroy project" do
+    it "returns http success" do
+      delete 'destroy'
+      response.should be_success
+    end
+  end
+
+end
