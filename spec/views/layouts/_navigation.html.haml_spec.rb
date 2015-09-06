4431b9e30487de09f8141e3554faa6d64501f005
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-06 18:31:26 +0400

    Add test for portfolio link in navigation

--- spec/views/layouts/_navigation.html.haml_spec.rb
+++ spec/views/layouts/_navigation.html.haml_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
 describe "layouts/_navigation.html.haml" do
-  it "should have link for index page" do
+  it "should have link to index page" do
     render
     expect(rendered).to have_link('Главная', '/')
   end
 
-  it "should have link for contacts page" do
+  it "should have link to contacts page" do
     render
     expect(rendered).to have_link('Контакты', '/contacts')
+  end
+
+  it "should have link to portfolio page" do
+    render
+    expect(rendered).to have_link('Портфолио', '/portfolio')
   end
 end
