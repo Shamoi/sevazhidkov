3e8149bd9eeacce5a4ebf1ef4f9f9e0724127886
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-09 20:16:57 +0400

    Add tests for correct project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "correct project" do
+    before do
+      @project = Project.create(name: 'Facedook', short_description: 'Your d.',
+                                description: 'Long description about project')
+    end
+
+    subject { @project }
+
+    it "should be valid" do
+      @project.should be_valid
+    end
+
+    it "should respond to name" do
+      @project.should respond_to(:name)
+    end
+
+    it "should respond to short_description" do
+      @project.should respond_to(:short_description)
+    end
+
+    it "should respond to full description" do
+      @project.should respond_to(:description)
+    end
+  end
 end
