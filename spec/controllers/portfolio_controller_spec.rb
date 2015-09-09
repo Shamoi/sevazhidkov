0f4e61edc2ad584409c0a350425dcf79be1e2b0e
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-09 19:39:02 +0400

    Update tests for portfolio controller

--- spec/controllers/portfolio_controller_spec.rb
+++ spec/controllers/portfolio_controller_spec.rb
@@ -5,50 +5,15 @@ describe PortfolioController do
   describe "index project" do
     it "returns http success" do
       get 'index'
-      response.should be_success
+      response.status.should == 200
     end
   end
 
   describe "show project" do
     it "returns http success" do
-      get 'show'
-      response.should be_success
+      get 'show', id: 1 # Need to run 'rake db:seed RAILS_ENV=test' before
+                        # running this test
+      response.status.should == 200
     end
   end
-
-  describe "create project" do
-    it "returns http success" do
-      post 'create'
-      response.should be_success
-    end
-  end
-
-  describe "new project" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "edit project" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "update project" do
-    it "returns http success" do
-      put 'update'
-      response.should be_success
-    end
-  end
-
-  describe "destroy project" do
-    it "returns http success" do
-      delete 'destroy'
-      response.should be_success
-    end
-  end
-
 end
