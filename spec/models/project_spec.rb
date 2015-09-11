30aadf589ddd972dc1a1b739fe31393240a426c5
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-11 21:59:40 +0400

    Format code for small monitors

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -28,7 +28,8 @@ describe Project do
 
   context "project with empty full description" do
     before do
-      @incorrect_project = Project.create(name: 'Facedook', short_description: 'Your d.',
+      @incorrect_project = Project.create(name: 'Facedook',
+                                          short_description: 'Your d.',
                                           description: '')
     end
 
@@ -41,7 +42,8 @@ describe Project do
 
   context "project with empty short description" do
     before do
-      @incorrect_project = Project.create(name: 'Facedook', short_description: '',
+      @incorrect_project = Project.create(name: 'Facedook',
+                                          short_description: '',
                                           description: 'Long story')
     end
 
@@ -54,7 +56,8 @@ describe Project do
 
   context "project with empty name description" do
     before do
-      @incorrect_project = Project.create(name: '', short_description: 'Your d.',
+      @incorrect_project = Project.create(name: '',
+                                          short_description: 'Your d.',
                                           description: 'Long story')
     end
 
