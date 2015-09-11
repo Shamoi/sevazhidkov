95205af9ddb1a42da5ec0c948c16105a2e7071f2
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-11 22:31:11 +0400

    Add validation for markdown description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,12 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :short_description, presence: true, length: { maximum: 146 }
+
+  # Rendered description in HTML
   validates :description, presence: true
+
+  # Project description in markdown
+  validates :markdown_description, presence: true
 
   protected
     def create_html_description_from_markdown
