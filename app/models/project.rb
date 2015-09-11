549d378632a40f0c695a82c6b6a50d1d72696b05
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-11 22:28:22 +0400

    Add method for creating html from markdown

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,11 @@ class Project < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :short_description, presence: true, length: { maximum: 146 }
   validates :description, presence: true
+
+  protected
+    def create_html_description_from_markdown
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                         autolink: true, tables: true)
+      self.description = markdown.render(self.markdown_description)
+    end
 end
