4e7a51ecdd039bf018dcf456bad71c987cc9c3d3
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-15 09:29:10 +0400

    Add helper for getting date of datetime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
       "#{page_title} — #{base_title}"
     end
   end
+
+  def get_date_of(datetime)
+    # If datetime is not nil, we convert it to needed time zone and format it
+    # If datetime is nil, we returned empty string.
+    if datetime
+      datetime.in_time_zone.strftime("%d.%m.%Y")
+    else
+      ""
+    end
+  end
 end
