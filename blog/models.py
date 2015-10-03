a5e4e121a7423d96fa9e7a7f26ac57aeba81ec40
Seva Zhidkov <zhidkovseva@gmail.com>
2015-10-03 08:48:08 +0400

    Install and add models for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Post(models.Model):
+    title = models.СharField(max_length=146)
+    preview_text = models.TextField()
+    preview_image = models.ImageField()
+    html_text = models.TextField()
+    markdown_text = models.TextField()
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField()
