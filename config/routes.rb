a856eed025a4f34f5ee136bd915de66e1ac30df1
Seva Zhidkov <zhidkovseva@gmail.com>
2016-01-06 00:10:39 +0300

    Delete last project

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'home#index', as: 'home'
-  get '/contact', to: 'static_pages#contact'
-  resources :portfolio, as: 'projects'
-end
