7bbe428ac9c4dfb5ccb6cfe5e8c40136bb4dc2c2
Seva Zhidkov <zhidkovseva@gmail.com>
2016-01-06 23:20:53 +0300

    Add page with count of words and symbols that I use in VK

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import render_template
 
 from . import app, cache
@@ -16,4 +18,16 @@ def places_index():
 
 @app.route('/word-count')
 def words_index():
-    return "Test"
+    words_stat = word_count.get_words(cache)
+    words = words_stat['stat']
+    # Calculate how much time left from last update of words data
+    words_update = int((time.time() - words_stat['updated']) // 3600)
+
+    symbols_stat = word_count.get_symbols(cache)
+    symbols = symbols_stat['stat']
+    symbols_update = int((time.time() - symbols_stat['updated']) // 3600)
+    return render_template(
+        'apps/word-count.html',
+        words=words, words_update=words_update,
+        symbols=symbols, symbols_update=symbols_update
+    )
