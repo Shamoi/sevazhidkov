35ce1e3ed71c866dc3c1e382c2a97081d132e698
Seva Zhidkov <zhidkovseva@gmail.com>
2016-01-07 15:22:20 +0300

    Add handling pull request events

--- app/apps/activity.py
+++ app/apps/activity.py
@@ -17,6 +17,7 @@ def get_github_activity(cache):
                 event_text = "Pushed {} commit to {} repository"
             else:
                 event_text = "Pushed {} commits to {} repository"
+
             repo_href = event['repo']['url']
             repo_href = repo_href.replace('api.', '').replace('repos/', '')
             repo_name = event['repo']['name']
@@ -27,18 +28,43 @@ def get_github_activity(cache):
             # just skip event
             if event['payload']['ref'] is not None:
                 continue
+
             event_text = 'Created {} repository'
+
             repo_href = event['repo']['url']
             repo_href = repo_href.replace('api.', '').replace('repos/', '')
             repo_name = event['repo']['name']
             repo_link = "<a href='{}'>{}</a>".format(repo_href, repo_name)
             activity.append(event_text.format(repo_link))
         if event['type'] == 'IssueCommentEvent':
-            event_text = 'Commented issue "{}"'
+            event_text = 'Commented issue "{}" in {}'
+
             issue_href = event['payload']['issue']['url']
             issue_href = issue_href.replace('api.', '').replace('repos/', '')
             issue_name = event['payload']['issue']['title']
             issue_link = "<a href='{}'>{}</a>".format(issue_href, issue_name)
-            activity.append(event_text.format(issue_link))
+
+            repo_href = event['repo']['url']
+            repo_href = repo_href.replace('api.', '').replace('repos/', '')
+            repo_name = event['repo']['name']
+            repo_link = "<a href='{}'>{}</a>".format(repo_href, repo_name)
+
+            activity.append(event_text.format(issue_link, repo_link))
+        if event['type'] == 'PullRequestEvent':
+            event_text = '{} pull request "{}" in {}'
+
+            event_type = event['payload']['action'].capitalize()
+
+            pr_href = event['payload']['pull_request']['issue_url']
+            pr_href = pr_href.replace('api.', '').replace('repos/', '')
+            pr_name = event['payload']['pull_request']['title']
+            pr_link = "<a href='{}'>{}</a>".format(pr_href, pr_name)
+
+            repo_href = event['repo']['url']
+            repo_href = repo_href.replace('api.', '').replace('repos/', '')
+            repo_name = event['repo']['name']
+            repo_link = "<a href='{}'>{}</a>".format(repo_href, repo_name)
+
+            activity.append(event_text.format(event_type, pr_link, repo_link))
         # TODO: IssueCreateEvent
-    return activity[:5]
+    return activity[:]
