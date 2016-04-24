919c3125455c2382a2a88536383a7fa04851c4f5
Seva Zhidkov <zhidkovseva@gmail.com>
2016-04-24 18:45:57 +0400

    Add basic homepage

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Hey!')
+    return render(request, 'home/index.html')
