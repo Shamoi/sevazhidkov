6f8190418ec9aa1fbc57b94671e3a38e038b82a5
Seva Zhidkov <zhidkovseva@gmail.com>
2016-05-14 10:06:12 +0400

    Add news parsing from RSS"

--- news/management/commands/parsenews.py
+++ news/management/commands/parsenews.py
@@ -1,14 +1,26 @@
 import time
+import datetime
+from dateutil import parser
 import feedparser
 from django.core.management.base import BaseCommand
 from news.models import Feed, Article
 
 
 class Command(BaseCommand):
-    help = 'Get news from RSS feeds and add it to DB'
+    help = 'Worker for getting news from RSS feeds and adding it to DB'
 
     def handle(self, *args, **options):
         while True:
             for feed in Feed.objects.all():
-                rss_articles = feedparser.parse(feed.rss_url)
-            time.sleep(5)
+                rss_articles = feedparser.parse(feed.rss_url)['entries']
+                for article_data in rss_articles:
+                    # If it's article with same url already exists, just pass it
+                    if Article.objects.filter(link=article_data['link']).exists():
+                        continue
+                    published_time = parser.parse(article_data['published'])
+                    article = Article(title=article_data['title'],
+                                      link=article_data['link'],
+                                      pub_date=published_time,
+                                      feed=feed)
+                    article.save()
+            time.sleep(30)
--- news/models.py
+++ news/models.py
@@ -7,11 +7,16 @@ class Feed(models.Model):
     name = models.CharField(max_length=50)
     rss_url = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     db_table = 'news_articles'
     title = models.CharField(max_length=300)
-    text = models.TextField()
     link = models.CharField(max_length=500)
     pub_date = models.DateTimeField(default=datetime.datetime.now)
     feed = models.ForeignKey(Feed)
+
+    def __str__(self):
+        return self.title
